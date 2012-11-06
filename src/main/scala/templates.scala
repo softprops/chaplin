1591e2521e426643dc3c32c1f651ed7e4d3483e1
softprops <d.tangren@gmail.com>
2012-11-06 18:56:20 -0500

    support implicit operator evaluation

--- src/main/scala/templates.scala
+++ src/main/scala/templates.scala
@@ -7,57 +7,70 @@ trait Template {
 }
 
 object Templates {
+  val Self = "."
+  def esc(in: String) = in // todo
   def mkTemplate(chunks: Seq[Chunk]): Template = new Template {
     def apply(view: View, writer: Writer) = {
-      def applyView(view: View, chunks: Seq[Chunk], writer: Writer): Unit = {
+      def applyValue(value: Value, chunks: Seq[Chunk], writer: Writer): Unit = {
         chunks match {
           case Nil => ()
           case head :: tail =>
-            def applyNext = applyView(view, tail, writer)
+            def applyNext(nextWriter: Writer) = applyValue(value, tail, nextWriter)
             head match {
               case Text(txt) =>
                 writer.write(txt)
-                applyNext
+                applyNext(writer)
               case Variable(name) =>
-                view(name).map {
-                  case StringVal(str) =>
-                    writer.write(str)
+                value match {
+                  case view:View => view(name).map {
+                    case StringVal(str) =>
+                      writer.write(esc(str))
+                    case _ => ()
+                  }
+                  case StringVal(str) if (Self == name) =>
+                    writer.write(esc(str))
                   case _ => ()
                 }
-                applyNext
+                applyNext(writer)
               case UnescapedVariable(name) =>
-                view(name).map {
-                  case StringVal(str) =>
+                value match {
+                  case view: View => view(name).map {
+                    case StringVal(str) =>
+                      writer.write(str)
+                    case _ => ()
+                  }
+                  case StringVal(str) if (Self == name) =>
                     writer.write(str)
                   case _ => ()
                 }
-                applyNext
+                applyNext(writer)
               case SectionOpen(name) =>
                 val (untilClose, afterClose) = tail.toList.span(!_.isInstanceOf[SectionClose])
-                view(name) match {                  
-                  case None => applyView(view, afterClose, writer)
-                  case Some(v:View) =>
-                    applyView(v, untilClose, writer)
-                  case Some(Falsy(bool)) =>
-                    if (!bool) applyView(view,
-                                         afterClose,
-                                         writer)
-                    else applyNext
-                  case Some(IterableVal(it)) =>
-                    it.foreach {
-                      case v: View => applyView(v, untilClose, writer)
-                      case StringVal(str) => writer.write(str)
-                      case _ => ()
-                    }
-                    applyView(view, afterClose, writer)
-                  case _ =>
-                    applyView(view, afterClose, writer)
+                value match {
+                  case view: View => view(name) match {
+                    case None =>
+                      applyValue(value, afterClose, writer)
+                    case Some(v: View) =>
+                      applyValue(v, untilClose, writer)
+                      applyValue(view, afterClose, writer)
+                    case Some(Falsy(bool)) =>
+                      if (!bool) applyValue(view,
+                                            afterClose,
+                                            writer)
+                      else applyNext(writer)
+                    case Some(IterableVal(it)) =>
+                      it.foreach(applyValue(_, untilClose, writer))
+                      applyValue(view, afterClose, writer)
+                    case _ =>
+                      applyValue(view, afterClose, writer)
+                  }
+                  case _ => ()
                 }
-              case SectionClose(_) => applyNext
+              case SectionClose(_) => applyNext(writer)
             }
         }
       }
-      applyView(view, chunks, writer)
+      applyValue(view, chunks, writer)
     }
     override def toString = chunks.toString
   }
