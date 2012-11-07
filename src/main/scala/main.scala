f1538b98ac5eccd5fd5aeb633e92af797acaea21
softprops <d.tangren@gmail.com>
2012-11-07 00:15:18 -0500

    moar examples

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,24 +1,45 @@
 package chaplin
 
+import java.io.{ PrintWriter, StringReader }
 object Main {
  def main(args: Array[String]) {
-    val template = """
-    | test {{#section}}
-    | {{greeting}} {{>part}} {{{name}}}
-    | {{/section}}""".stripMargin
-   def in = new java.io.StringReader(template)
-   println("default")
-   println(Mustache(in))
-   println("custom")
-   Mustache.dir("/custom/")(in).fold(identity, { template =>
-     println("template %s" format template)
+   //http://mustache.github.com/mustache.5.html
+
+   val typical = """Hello {{name}}
+   |You have just won ${{value}}!
+   |{{#in_ca}}
+   |Well, ${{taxed_value}}, after taxes.
+   |{{/in_ca}}""".stripMargin
+
+   Mustache(typical).fold(identity, { template =>
      val writer = new java.io.PrintWriter(System.out)
-     template(View().bind("section",
-                          View().bind("greeting", StringVal("hi"))
-                                .bind("name", StringVal("doug"))
-                                .bind("other_partial_var", StringVal("test partials"))),
-              writer)
-      writer.flush()
+     val view = View().bind("name", StringVal("chris"))
+                      .bind("value", StringVal("10000"))
+                      .bind("in_ca", Falsy(true))
+                      .bind("taxed_value", StringVal((10000 - (10000 * 0.4)).toString))
+     template(view, writer)
+     writer.flush()
    })
-  }
+
+   val inverted = """{{#repo}}
+   |<b>{{name}}</b>
+   |{{/repo}}
+   |{{^repo}}
+   |No repos :(
+   |{{/repo}}""".stripMargin
+
+   Mustache(inverted).fold(identity, { template =>
+     val writer = new PrintWriter(System.out)
+
+     // with repo
+     val view = View().bind("repo", View().bind("name", StringVal("chaplin")))
+     template(view, writer)
+     writer.flush()
+
+     // without repo
+     template(View(), writer)
+     writer.flush()
+   })
+
+ }
 }
