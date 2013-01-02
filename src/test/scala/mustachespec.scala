7d3183134e7e95aaf4788b8904b9b29d7cff57c0
softprops <d.tangren@gmail.com>
2013-01-02 08:37:27 -0500

    start using the mustache spec to test impl

--- src/test/scala/mustachespec.scala
+++ src/test/scala/mustachespec.scala
@@ -1,56 +1,44 @@
 package chaplin
 
-import org.scalatest.FlatSpec
+import org.scalatest.FunSpec//FlatSpec
 import java.io.StringWriter
 
-class MustacheSpec extends FlatSpec {
-  "mustache" should "render basic template" in {
-    val typical = """Hello {{name}}
-    |You have just won ${{value}}!
-    |{{#in_ca}}
-    |Well, ${{taxed_value}}, after taxes.
-    |{{/in_ca}}""".stripMargin
-    val writer = new StringWriter()
-    Mustache(typical).fold(identity, { template =>
-      val view = View().bind("name", StringVal("chris"))
-                      .bind("value", StringVal("10000"))
-                      .bind("in_ca", Falsy(true))
-                      .bind("taxed_value", StringVal((10000 - (10000 * 0.4)).toString))
-     template(view, writer).flush()
-     assert(writer.toString() === """Hello chris
-            |You have just won $10000!
-            |
-            |Well, $6000.0, after taxes.
-            |""".stripMargin)
-   })
+import dispatch._
+import net.liftweb.json._
+
+class MustacheSpec extends FunSpec {
+  val SpecBase = "https://raw.github.com/mustache/spec/master/specs"
+  val Features = "interpolation" :: Nil //:: "inverted" :: "interpolation" :: "partials" :: "sections" :: Nil
+  val promises = Features map { f =>
+    for {
+      js <- Http(url(SpecBase) / "%s.json".format(f) > as.lift.Json)
+    } yield {
+      describe(f) {
+        for {
+          JObject(fields)                       <- js
+          JField("tests", JArray(tests))        <- fields
+          JObject(testfields)                   <- tests
+          JField("name", JString(name))         <- testfields
+          JField("expected", JString(expected)) <- testfields
+          JField("template", JString(template)) <- testfields
+          JField("desc", JString(desc))         <- testfields
+          JField("data", data)                   <- testfields
+        } yield {
+          it("%s: %s" format(name, desc)) {
+            val writer = new StringWriter()
+            val view = Views.of(data) match {
+              case Some(v:View) => v
+              case _ => View()
+            }
+            Mustache(template).fold(identity, { must =>
+              must(view, writer).flush()
+              assert(writer.toString() === expected)
+            })
+          }
+        }
+      }
+    }
   }
 
-  it should "render an inverted template"  in  {
-    val inverted = """{{#repo}}
-    |<b>{{name}}</b>
-    |{{/repo}}
-    |{{^repo}}
-    |No repos :(
-    |{{/repo}}""".stripMargin
-
-    val invertedStache = Mustache(inverted)
-    val writer = new StringWriter()
-    invertedStache.fold(identity, { template =>
-      val view = View().bind("repo", View().bind("name", StringVal("chaplin")))
-      template(view, writer)
-    })
-    assert(writer.toString() === """
-           |<b>chaplin</b>
-           |
-           |""".stripMargin)
-  }
-
-  it should "not render comments" in {
-    val comment = "<h1>Today{{! ignore me }}.</h1>"
-    val writer = new StringWriter()
-    Mustache(comment).fold(identity, { template =>
-      template(View(), writer).flush()
-    })
-    assert(writer.toString() === "<h1>Today.</h1>")
-  }
+  Http.promise.all(promises)()
 }
